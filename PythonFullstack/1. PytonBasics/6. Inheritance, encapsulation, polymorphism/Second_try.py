81768e81b2199a615e30d7406c6977767daa781d
Ivan Gordeev <ivangordeev2001@mail.ru>
2023-01-14 21:28:30 +0300

    7,8 Hometasks in Python Basics

---
+++ PythonFullstack/1. PytonBasics/6. Inheritance, encapsulation, polymorphism/Second_try.py
@@ -0,0 +1,57 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and (course in self.finished_courses or course in self.courses_in_progress) and (grade >= 0 or grade <= 10):
+            lecturer.grades[course] += [grade]
+        else:
+            return 'ошибка'
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+good_mentor = Lecturer('Once', 'Told')
+good_mentor.courses_attached += ['Python']
+
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+print(best_student.grades)
+
+best_student.rate_lecturer(good_mentor, 'Python', 10)
+
+print(good_mentor.grades)
---
+++ PythonFullstack/1. PytonBasics/7. File system/First_try.py
@@ -0,0 +1,62 @@
+print('\nЗадание 1')
+cook_book = []
+
+with open('data.txt', 'r', encoding = 'utf8') as cook_list:
+    for line in cook_list:
+        dish_name = line.strip()
+        ingredients = []
+        dish = {dish_name: ingredients}
+        ing_count = cook_list.readline()
+        for i in range(int(ing_count)):
+            ing = cook_list.readline()
+            ingredient_name, quantity, measure = ing.split(' | ')
+            ingred = {'ingredient_name': ingredient_name, 'quantity': int(quantity), 'measure': measure.rstrip('\n')}
+            ingredients.append(ingred)
+        cook_list.readline()
+        cook_book.append(dish)
+
+for n in range(len(cook_book)):
+    print(f'{cook_book[n]}\n')
+
+print('\nЗадание 2')
+result = {}  
+def get_shop_list_by_dishes(dishes, person_count):
+    for recipes in cook_book:
+        for dish in dishes:
+            if dish in recipes.keys():
+                for ing in recipes[dish]:
+                    dict = {ing['ingredient_name']: {'measure': ing['measure'], 'quantity': person_count*ing['quantity']}}
+                    if result.get(ing['ingredient_name']):
+                        extra_item = (int(result[ing['ingredient_name']]['quantity']) +
+                                  int(result[ing['ingredient_name']]['quantity']))
+                        result[ing['ingredient_name']]['quantity'] = extra_item
+                    else:
+                        result.update(dict)
+    print(result)            
+
+get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+print('\nЗадание 3')
+
+import os
+
+def make_file(file1, file2, file3):
+    counter = []
+    for file in (file1, file2, file3):
+        file_name = file
+        with open(file, encoding='utf8') as files:
+            content_file = files.readlines()
+            counter.append([file_name, content_file])
+
+    counter.sort(key=lambda x: len(x[1]))
+
+    with open('4.txt', 'w', encoding='utf8') as files:
+        for file in counter:
+            res = f'{file[0]}\n{len(file[1])}\n{"".join(file[1])}\n\n'
+            files.writelines(res)
+
+    with open('4.txt', 'r', encoding='utf8') as files:
+        content = files.read()
+        print(content)
+
+make_file('1.txt', '2.txt', '3.txt')
