162c82aee3dbeb63b56fe724d40a307ef9c5128a
BountyBreaker <111979957+IvanGordeevPy@users.noreply.github.com>
2023-03-15 03:12:32 +0300

    Update Task 1.2.sql

--- PythonFullstack/3. Databases/2 PostgreSQL/Task 1.2.sql
+++ PythonFullstack/3. Databases/2 PostgreSQL/Task 1.2.sql
@@ -1,46 +1,46 @@
 CREATE TABLE IF NOT EXISTS Genres (
-	id INTEGER PRIMARY KEY,
-	name VARCHAR(60) NOT NULL
+	id SERIAL PRIMARY KEY,
+	genre_name VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS performer (
-	performerID INTEGER PRIMARY KEY,
-	alias VARCHAR(40) NOT NULL
+CREATE TABLE IF NOT EXISTS Performers (
+	id SERIAL PRIMARY KEY,
+	alias VARCHAR(50) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS album (
-	albumID INTEGER PRIMARY KEY,
-	album_title VARCHAR(80) NOT NULL,
-	release_year INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS Albums (
+	id SERIAL PRIMARY KEY,
+	album_title VARCHAR(60) UNIQUE NOT NULL,
+	year TINYINT(4) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS genreperformer (
-	genreID INTEGER REFERENCES genre(genreID),
-	performerID INTEGER REFERENCES performer(performerID),
-	CONSTRAINT pk_genreperformer PRIMARY KEY (genreID, performerID)
+CREATE TABLE IF NOT EXISTS PerformersAllGenres (
+	performer_id INTEGER REFERENCES Performers(id),
+	genre_id INTEGER REFERENCES Genres(id),
+	CONSTRAINT gp PRIMARY KEY (preformer_id, genre_id)
 );
 
-CREATE TABLE IF NOT EXISTS performeralbum (
-	albumID INTEGER REFERENCES album(albumID),
-	performerID INTEGER REFERENCES performer(performerID),
-	constraint pk_performeralbum PRIMARY KEY (albumID, performerID)
+CREATE TABLE IF NOT EXISTS PerformersAllAlbums (
+	performer_id INTEGER REFERENCES Performers(id),
+	album_id INTEGER REFERENCES Albums(id),
+	constraint pa PRIMARY KEY (performer_id, album_id)
 );
 
-CREATE TABLE IF NOT EXISTS track (
-	trackID INTEGER PRIMARY KEY,
-	albumID INTEGER REFERENCES album(albumID),
-	track_title VARCHAR(80) not null,
-	duration TIME not NULL
+CREATE TABLE IF NOT EXISTS Tracks (
+	id SERIAL PRIMARY KEY,
+	track_title VARCHAR(80) NOT NULL,
+	duration TIME not NULL CHECK(duration > 0),
+	album_id INTEGER REFERENCES Albums(album_id)
 );
 
-CREATE TABLE IF NOT EXISTS music_collection (
-	collectionID INTEGER PRIMARY KEY,
-	collection_title VARCHAR(80) not null,
-	release_year INTEGER not null
+CREATE TABLE IF NOT EXISTS Collections (
+	id SERIAL PRIMARY KEY,
+	collection_title VARCHAR(80) NOT NULL,
+	year TINYINT(4) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS trackcollection (
-	trackID INTEGER REFERENCES track(trackID),
-	collectionID INTEGER REFERENCES music_collection(collectionID),
-	CONSTRAINT pk_trackcollection PRIMARY KEY (trackID, collectionID)
+CREATE TABLE IF NOT EXISTS TracksInCollections (
+	collection_id INTEGER REFERENCES Collections(id),
+	track_id INTEGER REFERENCES Tracks(id),
+	CONSTRAINT ct PRIMARY KEY (collection_id, track_id)
 );
